f13c54a9c315a34d360bedc7311e6b72b465ba14
antecanis8 <42382878+antecanis8@users.noreply.github.com>
2025-05-22 23:02:17 +0000

    补充更改faraday版本

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@
 # url: https://moderationapi.com
 # required_version: 2.7.0
 
-gem "faraday", "2.12.2", { require: false }
+gem "faraday", "2.13.1", { require: false }
 gem "faraday-net_http", "3.4.0", { require: false }
 gem "multipart-post", "2.4.1", { require: false }
 gem "faraday-multipart", "1.1.0", { require: false }
